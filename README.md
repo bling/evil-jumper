f9ce03e42da353f78e34a91815e9d72da3c4c763
Bailey Ling <bling@live.ca>
2014-07-02 12:17:41 -0600

    first commit/implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+evil-jumper
+===========
+
+This changes the jump list behavior in evil-mode to behave more like Vim; specifically, it will jump across buffer boundaries, even if they are dead.
+
+installation
+============
+
+Add `evil-jumper.el` to the `load-path` and `(require 'evil-jumper)`.
+
+usage
+=====
+
+Requiring the file will automatically set up `C-i` and `C-o`.
---
+++ evil-jumper.el
@@ -0,0 +1,121 @@
+;;; evil-jumper.el --- Jump like vimmers do!
+
+;; Copyright (C) 2014 by Bailey Ling
+;; Author: Bailey Ling
+;; URL: https://github.com/bling/evil-jumper
+;; Filename: evil-jumper.el
+;; Description: Jump like vimmers do!
+;; Created: 2014-07-01
+;; Version: 0.0.1
+;; Keywords: evil vim jumplist jump list
+;; Package-Requires: ((evil "0"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Commentary:
+;;
+;; Install:
+;; (require 'evil-jumper)
+;;
+;; Usage:
+;;
+;; Requiring will automatically rebind C-o and C-i.
+
+;;; Code:
+
+(require 'evil)
+
+(defgroup evil-jumper nil
+  "evil-jumper configuration options."
+  :prefix "evil-jumper"
+  :group 'evil)
+
+(defcustom evil-jumper-max-length 100
+  "The maximum number of jumps to keep track of."
+  :type 'integer
+  :group 'evil-jumper)
+
+(defcustom evil-jumper-auto-center nil
+  "Auto-center the line after jumping."
+  :type 'boolean
+  :group 'evil-jumper)
+
+(defvar evil-jumper--list nil)
+(defvar evil-jumper--idx -1)
+(defvar evil-jumper--jumping nil)
+
+(defun evil-jumper--jump-to-index (idx)
+  (when (>= idx (length evil-jumper--list))
+    (setq idx 0))
+  (when (< idx 0)
+    (setq idx (- (length evil-jumper--list) 1)))
+  (setq evil-jumper--idx idx)
+  (let* ((place (nth idx evil-jumper--list))
+         (pos (car place))
+         (file-name (cadr place)))
+    (setq evil-jumper--jumping t)
+    (if (equal file-name "*scratch*")
+        (switch-to-buffer file-name)
+      (find-file file-name))
+    (setq evil-jumper--jumping nil)
+    (goto-char pos)
+    (when evil-jumper-auto-center
+      (recenter))))
+
+(defun evil-jumper--push ()
+  (while (> (length evil-jumper--list) evil-jumper-max-length)
+    (nbutlast evil-jumper--list 1))
+  (let ((file-name (buffer-file-name))
+        (buffer-name (buffer-name)))
+    (if file-name
+        (push `(,(point) ,file-name) evil-jumper--list)
+      (when (equal buffer-name "*scratch*")
+        (push `(,(point) ,buffer-name) evil-jumper--list)))))
+
+(defun evil-jumper--set-jump ()
+  ;; clear out intermediary jumps when a new one is set
+  (nbutlast evil-jumper--list evil-jumper--idx)
+  (setq evil-jumper--idx -1)
+  (evil-jumper--push))
+
+(evil-define-motion evil-jumper/backward (count)
+  (let ((count (or count 1)))
+    (evil-motion-loop (nil count)
+      (when (= evil-jumper--idx -1)
+        (incf evil-jumper--idx)
+        (evil-jumper--push))
+      (evil-jumper--jump-to-index (+ evil-jumper--idx 1)))))
+
+(evil-define-motion evil-jumper/forward (count)
+  (let ((count (or count 1)))
+    (evil-motion-loop (nil count)
+      (evil-jumper--jump-to-index (- evil-jumper--idx 1)))))
+
+(defadvice evil-set-jump (after advice-for-evil-set-jump activate)
+  (evil-jumper--set-jump))
+
+(defadvice find-file-noselect (after advice-for-find-file-noselect activate)
+  (unless evil-jumper--jumping
+    (evil-jumper--set-jump)))
+
+(define-key evil-motion-state-map (kbd "C-o") 'evil-jumper/backward)
+(define-key evil-motion-state-map (kbd "C-i") 'evil-jumper/forward)
+
+(provide 'evil-jumper)
